f392c749203735cd9368da14c39dfe8211ca8c29
= <=>
2024-11-12 02:23:31 +0330

    Authentication Using Clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,11 @@
 // import Image from "next/image";
-import { Button } from "@/components/ui/button";
+
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-    <p className="text-3xl font-bold text-indigo-500">hello Dislor Clone</p>
-    <Button variant="ghost" className="bg-indigo-500">click me</Button>
+      <UserButton />
     </div>
   );
 }
